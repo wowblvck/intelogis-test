5d92c60859b6f32d1cea2540a5ed99544e40dddb
Indar Basto <indar.basto@gmail.com>
2023-07-28 19:14:52 +0300

    feat: add map

---
+++ src/utils/map.ts
@@ -0,0 +1,17 @@
+const calculateCenter = <T extends { location: [number, number] }>(
+  points: T[]
+): [number, number] => {
+  const numPoints = points.length;
+  if (numPoints === 0) return [0, 0];
+
+  const center = points.reduce(
+    (accumulator, point) => {
+      return [accumulator[0] + point.location[0], accumulator[1] + point.location[1]];
+    },
+    [0, 0]
+  );
+
+  return [center[0] / numPoints, center[1] / numPoints];
+};
+
+export default calculateCenter;
