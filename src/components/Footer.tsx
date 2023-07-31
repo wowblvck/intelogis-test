893ddaad902f53042175b249c4b3d587d6751660
Indar Basto <indar.basto@gmail.com>
2023-07-31 14:30:59 +0300

    feat: add author references

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,13 @@
+import { Grid } from 'antd';
 import { Footer as FooterAnt } from 'antd/es/layout/layout';
 import Space from 'antd/es/space';
 import Typography from 'antd/es/typography';
+import Link from 'antd/es/typography/Link';
+
+const { useBreakpoint } = Grid;
 
 const Footer: React.FC = () => {
+  const { xs } = useBreakpoint();
   return (
     <FooterAnt
       style={{
@@ -11,10 +16,31 @@ const Footer: React.FC = () => {
         width: '100%',
       }}
     >
-      <Space style={{ justifyContent: 'space-between', width: '100%' }}>
-        <Typography.Text>2023</Typography.Text>
-        <Typography.Text>Indar Basto (wowblvck)</Typography.Text>
-        <Typography.Text>Github</Typography.Text>
+      <Space
+        align="center"
+        direction={xs ? 'vertical' : 'horizontal'}
+        style={{ justifyContent: 'space-between', width: '100%' }}
+      >
+        <Typography.Text type="secondary">2023</Typography.Text>
+        <Link href="https://wowblvck.tech/" target="_blank">
+          Indar Basto (wowblvck)
+        </Link>
+        <Link href="https://github.com/wowblvck" target="_blank">
+          <div
+            style={{
+              alignItems: 'center',
+              columnGap: '5px',
+              display: 'flex',
+            }}
+          >
+            <img
+              alt="Github"
+              src="https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg"
+              style={{ height: '24px', lineHeight: 0, margin: 0, padding: 0, width: '24px' }}
+            />
+            Github
+          </div>
+        </Link>
       </Space>
     </FooterAnt>
   );
