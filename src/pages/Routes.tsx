32b21db8b026bb926d22e5b8dee1bd7acf55c6a0
Indar Basto <indar.basto@gmail.com>
2023-07-31 14:19:06 +0300

    feat: add routes list responsive

--- src/pages/Routes.tsx
+++ src/pages/Routes.tsx
@@ -2,17 +2,20 @@ import RoutesList from '@components/RoutesList';
 import { clearCurrentRoute, fetchCurrentRoute } from '@reducers/route.reducer';
 import { fetchRoutesStart } from '@reducers/routes.reducer';
 import { useAppDispatch, useAppSelector } from '@store/hooks';
-import { Col, Row, Spin, notification } from 'antd';
+import { Col, Grid, Row, Spin, notification } from 'antd';
 import { Suspense, lazy, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 const Map = lazy(() => import('@components/Map'));
+
+const { useBreakpoint } = Grid;
 
 const Routes: React.FC = () => {
   const { id } = useParams<{ id: string }>();
   const { data, error, loading } = useAppSelector((state) => state.routesReducer);
   const { currentRoute, error: routeError } = useAppSelector((state) => state.routeReducer);
   const dispatch = useAppDispatch();
+  const { xl } = useBreakpoint();
 
   useEffect(() => {
     if (error || routeError) {
@@ -42,12 +45,20 @@ const Routes: React.FC = () => {
     }
   }, [id, data]);
   return (
-    <Row gutter={16} style={{ height: '100%' }}>
-      <Col span={currentRoute ? 6 : 24}>
+    <Row align="top" gutter={16} style={{ height: '100%', overflow: 'hidden' }}>
+      <Col span={currentRoute && xl ? 6 : 24}>
         <RoutesList loading={loading} source={data!} />
       </Col>
       {currentRoute && (
-        <Col span={18} style={{ alignItems: 'center', display: 'flex', justifyContent: 'center' }}>
+        <Col
+          style={{
+            alignItems: 'center',
+            display: 'flex',
+            height: '100%',
+            justifyContent: 'center',
+          }}
+          span={xl ? 18 : 24}
+        >
           <Suspense fallback={<Spin />}>
             <Map route={currentRoute} />
           </Suspense>
