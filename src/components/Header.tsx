bd04134f63765913056dcd5efda7d908c6617070
Indar Basto <indar.basto@gmail.com>
2023-07-31 14:49:19 +0300

    feat: add page name

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,10 @@
 import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons/lib/icons';
+import { appRoutes } from '@routes/appRoutes';
+import { getDefaultPath } from '@utils/helperFunctions';
+import { Typography } from 'antd';
 import Button from 'antd/es/button';
 import { Header as HeaderAnt } from 'antd/es/layout/layout';
+import { useLocation, useParams } from 'react-router-dom';
 
 type HeaderProps = {
   menuCollapsed: boolean;
@@ -8,12 +12,20 @@ type HeaderProps = {
 };
 
 const Header: React.FC<HeaderProps> = ({ menuCollapsed, onCollapsed }) => {
+  const location = useLocation();
+  const params = useParams<Record<string, string>>();
+
+  const defaultPath = getDefaultPath(location, params);
+
+  const page = appRoutes.find((route) => route.path === defaultPath);
+
   return (
     <HeaderAnt
       style={{
         alignItems: 'center',
         backgroundColor: '#fff',
         boxShadow: '0px 2px 8px 0px rgba(34, 60, 80, 0.2)',
+        columnGap: '10px',
         display: 'flex',
         padding: 0,
       }}
@@ -28,6 +40,9 @@ const Header: React.FC<HeaderProps> = ({ menuCollapsed, onCollapsed }) => {
         onClick={onCollapsed}
         type="text"
       />
+      {page && (
+        <Typography.Title style={{ fontSize: '24px', margin: 0 }}>{page.name}</Typography.Title>
+      )}
     </HeaderAnt>
   );
 };
